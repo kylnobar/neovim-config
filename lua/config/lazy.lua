da855a10362d3bcafddd11a6965149968cfac3ae
Casey Sorrells <csorrells@blackoakcasino.com>
2025-08-14 14:47:45 -0700

    changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,3 +1,52 @@
+-- -- ~/.config/nvim/init.lua
+--
+-- -- Bootstrap lazy.nvim
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- local gitconfig_content = [[
+-- [url "https://github.com/"]
+--   insteadOf = git@github.com:
+-- ]]
+--
+-- if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+--   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+--   if vim.v.shell_error ~= 0 then
+--     vim.api.nvim_echo({
+--       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+--       { out, "WarningMsg" },
+--       { "\nPress any key to exit..." },
+--     }, true, {})
+--     vim.fn.getchar()
+--     os.exit(1)
+--   end
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+--
+-- -- Make sure to setup `mapleader` and `maplocalleader` before
+-- -- loading lazy.nvim so that mappings are correct.
+-- -- This is also a good place to setup other settings (vim.opt)
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
+--
+-- -- Setup lazy.nvim
+-- require("lazy").setup({
+--   spec = {
+--     -- import your plugins
+--     { import = "plugins" },
+--   },
+--   -- Configure any other settings here. See the documentation for more details.
+--   install = { colorscheme = { "ellisonleao/gruvbox.nvim" } },
+--   -- automatically check for plugin updates
+--   checker = { enabled = true },
+--   -- Configure Git to use the inline config for all operations
+--   git = {
+--     config = function()
+--       return gitconfig_content
+--     end,
+--     -- Explicitly set the default protocol to HTTPS
+--     default_url_format = "https://github.com/%s.git",
+--   },
+-- })
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
